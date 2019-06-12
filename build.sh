5d58f520af7f978142792d88ae58d6c4806d11d8
Artanidos <artanidos@gmail.com>
2019-06-12 18:38:53 +0200

    deploy plugins

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
 pyinstaller main.py
-=======
-pyinstaller --onefile main.py
->>>>>>> 5c5a3f268fe94783ab1d4aa10577d832a9080e51
+mkdir dist/main/plugins
+cp plugins/*.py dist/main/plugins
