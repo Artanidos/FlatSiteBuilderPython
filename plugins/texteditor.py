0f20ce228514359d91a7bb518cccc4b27ad2ca34
Artanidos <artanidos@gmail.com>
2019-06-13 18:31:27 +0200

    hiliter added

--- plugins/texteditor.py
+++ plugins/texteditor.py
@@ -29,14 +29,14 @@ from widgets.roweditor import RowEditor
 from widgets.columneditor import ColumnEditor
 from widgets.elementeditor import ElementEditor, Mode
 from widgets.content import ContentType
-from widgets.xmlhighlighter import XmlHighlighter
 from widgets.interfaces import ElementEditorInterface
 from widgets.item import Item
 from PyQt5.QtWidgets import QUndoStack, QHBoxLayout, QTextEdit, QVBoxLayout, QGridLayout, QLabel, QPushButton, QLineEdit, QComboBox, QScrollArea
-from PyQt5.QtCore import Qt, QUrl, pyqtSignal, qVersion, qRegisterResourceData, qUnregisterResourceData
-from PyQt5.QtGui import QFont, QFontMetrics, QImage
+from PyQt5.QtCore import Qt, QUrl, QRegExp, pyqtSignal, qVersion, qRegisterResourceData, qUnregisterResourceData
+from PyQt5.QtGui import QFont, QFontMetrics, QImage, QSyntaxHighlighter, QTextCharFormat, QColor
 from PyQt5.QtQml import qmlRegisterType
 import resources
+
 
 class TextEditor(ElementEditorInterface):
     close = pyqtSignal()
@@ -154,6 +154,139 @@ class Text(Item):
 
     def getHtml(self):
         return html.unescape(self.text)
+
+
+ENTITY = 0
+TAG = 1
+CODE = 2
+COMMENT = 3
+LAST_CONSTRUCT = COMMENT
+
+
+NORMAL_STATE = -1
+IN_COMMENT = 0
+IN_TAG = 1
+IN_VAR = 2
+IN_LOOP = 3
+
+
+class XmlHighlighter(QSyntaxHighlighter):
+    def __init__(self, parent = None):
+        super(XmlHighlighter, self).__init__(parent)
+
+        self.formats = [0] * (LAST_CONSTRUCT + 1)
+        entityFormat = QTextCharFormat()
+        entityFormat.setForeground(QColor(0, 128, 0))
+        entityFormat.setFontWeight(QFont.Normal)
+        self.setFormatFor(ENTITY, entityFormat)
+
+        tagFormat = QTextCharFormat()
+        tagFormat.setForeground(QColor("#f0e68c"))
+        tagFormat.setFontWeight(QFont.Normal)
+        self.setFormatFor(TAG, tagFormat)
+
+        commentFormat = QTextCharFormat()
+        commentFormat.setForeground(QColor("#87ceeb"))
+        commentFormat.setFontItalic(True)
+        self.setFormatFor(COMMENT, commentFormat)
+
+        codeFormat = QTextCharFormat()
+        codeFormat.setForeground(QColor("#ff9e00"))
+        self.setFormatFor(CODE, codeFormat)
+
+    def setFormatFor(self, construct, format):
+        self.formats[construct] = format
+        self.rehighlight()
+
+    def formatFor(self, construct):
+        return self.formats[construct]
+
+    def highlightBlock(self, text):
+        state = self.previousBlockState()
+        length = len(text)
+        start = 0
+        pos = 0
+
+        while pos < length:
+            if state == IN_VAR:
+                start = pos
+                while pos < length:
+                    if text[pos:pos + 2] == "":
+                        pos += 2
+                        state = NORMAL_STATE
+                        break
+                    else:
+                        pos = pos + 1
+                self.setFormat(start, pos - start, self.formats[CODE])
+
+            elif state == IN_LOOP:
+                start = pos
+                while pos < length:
+                    if text[pos: pos + 2] == "%":
+                        pos += 2
+                        state = NORMAL_STATE
+                        break
+                    else:
+                        pos = pos + 1
+                self.setFormat(start, pos - start, self.formats[CODE])
+
+            elif state == IN_COMMENT:
+                start = pos
+                while pos < length:
+                    if text[pos: pos + 3] == "-->":
+                        pos += 3
+                        state = NORMAL_STATE
+                        break
+                    else:
+                        pos = pos + 1
+                self.setFormat(start, pos - start, self.formats[COMMENT])
+
+            elif state == IN_TAG:
+                quote = 0
+                start = pos
+                while pos < length:
+                    ch = text[pos]
+                    if quote == 0:
+                        if ch == "\"" or ch == '"':
+                            quote = ch
+                        elif ch == '>':
+                            pos = pos + 1
+                            state = NORMAL_STATE
+                            break
+                    elif ch == quote:
+                        quote = 0
+                    pos = pos + 1
+                self.setFormat(start, pos - start, self.formats[TAG])
+
+            else:
+                while pos < length:
+                    ch = text[pos]
+                    if ch == '<':
+                        if text[pos: pos + 4] == "<!--":
+                            state = IN_COMMENT
+                        else:
+                            state = IN_TAG
+                        break
+                        
+                    elif ch == '&':
+                        start = pos
+                        while pos < length and text[pos] != '':
+                            pos = pos + 1
+                                
+                        self.setFormat(start, pos - start, self.formats[ENTITY])
+                    elif ch == "":
+                        if text[pos: pos + 2] == "":
+                            state = IN_VAR
+                            break
+                        elif text[pos: pos + 2] == "%":
+                            state = IN_LOOP
+                            break
+                        pos = pos + 1
+                        break
+                    else:
+                        pos = pos +1
+
+        self.setCurrentBlockState(state)
 
 
 qt_resource_data = b"\
