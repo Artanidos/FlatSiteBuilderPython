a6aa44e7a18c7e48793fdd5602ce01beb355c1a8
Artanidos <artanidos@gmail.com>
2019-12-02 18:36:00 +0100

    upd

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 rm -r dist/*
 rm -r packages/com.vendor.product/data/*
 pyinstaller main.py
+mkdir packages/com.vendor.product/data
 mkdir packages/com.vendor.product/data/plugins
 mkdir packages/com.vendor.product/data/themes
 mkdir packages/com.vendor.product/data/sources
@@ -8,5 +9,5 @@ cp -r dist/main/* packages/com.vendor.product/data
 cp plugins/*.py packages/com.vendor.product/data/plugins
 cp -r themes/* packages/com.vendor.product/data/themes
 mv packages/com.vendor.product/data/main packages/com.vendor.product/data/FlatSiteBuilder
-/home/art/Qt/Tools/QtInstallerFramework/3.1/bin/binarycreator -f -c config/config.xml -p packages FlatSiteBuilder-Linux-2.0.0.Setup
+/home/art/Qt/Tools/QtInstallerFramework/3.1/bin/binarycreator -f -c config/config.xml -p packages FlatSiteBuilder-Linux-2.1.0.Setup
 
